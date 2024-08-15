23053da57d47a6d3a4c2a7c87f93ed248b58065e
ukaushik <ukaushik@ebay.com>
2024-08-14 23:56:23 -0700

    pass stage

--- src/concurrent.rs
+++ src/concurrent.rs
@@ -1,14 +1,16 @@
 use core::str;
 
-use tokio::{io::{AsyncReadExt, AsyncWriteExt}, net::TcpListener};
+use tokio::{io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader}, net::TcpListener};
 
 pub async fn concurrent() {
     let listener = TcpListener::bind("127.0.0.1:4221").await.unwrap();
     loop {
         let (mut stream, _) = listener.accept().await.unwrap();
         tokio::spawn(async move {
-            let mut buf = vec![];
-            stream.read_buf(&mut buf).await.unwrap();
+            let mut buf = Vec::with_capacity(1024);
+            let mut buf_reader = BufReader::new(&mut stream);
+            buf_reader.read_buf(&mut buf).await.unwrap();
+                
 
             let incoming_request: Vec<&str> = str::from_utf8(&buf).unwrap().split("\r\n").collect();
             let path: Vec<&str> = incoming_request[0].split_ascii_whitespace().collect();
@@ -24,6 +26,14 @@ pub async fn concurrent() {
                             Ok(_) => println!("SUCCESFULLY ECHOED: {}", echo_val),
                             Err(_) => println!("FAILED TO WRITE RESPONSE!"),
                         }
+                    } else if path[1][..11].to_string() == "/user-agent" {
+                        let header_vec: Vec<&str> = incoming_request[3].split(" ").collect(); 
+                        let header_val = header_vec[1];
+                        let res_body = format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}\r\n", header_val.len(), header_val);
+                        match stream.write_all(res_body.as_bytes()).await {
+                            Ok(_) => println!("SUCCESFULLY ECHOED: {}", header_val),
+                            Err(_) => println!("FAILED TO WRITE RESPONSE!"),
+                        } 
                     } else {
                         stream.write_all(b"HTTP/1.1 404 Not Found\r\n\r\n").await;
                     }
