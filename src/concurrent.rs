9325f60d5375630ce2380b5c4ad45d154b277bef
ukaushik <ukaushik@ebay.com>
2024-08-14 21:44:00 -0700

    pass stage

--- src/concurrent.rs
+++ src/concurrent.rs
@@ -12,13 +12,12 @@ pub async fn concurrent() {
 
             let incoming_request: Vec<&str> = str::from_utf8(&buf).unwrap().split("\r\n").collect();
             let path: Vec<&str> = incoming_request[0].split_ascii_whitespace().collect();
-            println!("INCOMING REQUEST: {:?}", incoming_request);
             
             match path[0] {
                 "GET" => {
                     if path[1][..6].to_string() == "/echo/" {
                         let echo_val = path[1][6..].to_string();
-                        let res_body = format!("HTTP/1.1 200 OK\\r\\nContent-Type: text/plain\\r\\nContent-Length: {}\\r\\n\\{}\\r\\n", echo_val.len(), echo_val);
+                        let res_body = format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", echo_val.len(), echo_val);
                         stream.write_all(res_body.as_bytes()).await
                     } else if path[1] == "/" {
                         stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n").await
